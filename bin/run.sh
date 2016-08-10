58dee910d7dd2b52cbcb7dc4c4d7be6f76c20146
MADHUKAR THOTA <Madhukar.Thota@LibertyMutual.com>
2016-08-09 22:31:09 -0400

    update

--- bin/run.sh
+++ bin/run.sh
@@ -4,5 +4,4 @@ set -euo pipefail
 
 export APP_ROOT=$HOME
 echo $(ls -l ${APP_ROOT}/grafana/config)
-echo ${APP_ROOT}/grafana/bin/grafana-server --config=${APP_ROOT}/grafana/config/grafana.ini
-exec ${APP_ROOT}/grafana/bin/grafana-server --config=${APP_ROOT}/grafana/config/grafana.ini
+exec ${APP_ROOT}/grafana/bin/grafana-server --port $PORT
